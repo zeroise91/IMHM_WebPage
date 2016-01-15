3f2c206bcc47d1ac4a3f29790d998dfa15b39ae3
ParkBeomChan <zeroise91@gmail.com>
2016-01-16 05:58:24 +0900

    test

--- src/DB/DTO/searchOffsetDTO.java
+++ src/DB/DTO/searchOffsetDTO.java
@@ -8,6 +8,7 @@ public class searchOffsetDTO {
     private Integer limit = null;
     private Integer offset=null;
 
+
     public searchOffsetDTO(String id, Integer limit, Integer offset) {
         this.id = id;
         this.limit = limit;
